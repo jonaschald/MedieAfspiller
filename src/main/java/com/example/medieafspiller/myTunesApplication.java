890ff5dbadafa8fe3d7d54021851b17e3ae9412c
Jonas <jonhal01@easv365.dk>
2025-11-27 12:46:03 +0100

    Empty all

--- src/main/java/com/example/medieafspiller/myTunesApplication.java
+++ src/main/java/com/example/medieafspiller/myTunesApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class myTunesApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(myTunesApplication.class.getResource("myTunes.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("myTunes");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/medieafspiller/myTunesController.java
@@ -0,0 +1,5 @@
+package com.example.medieafspiller;
+
+public class myTunesController {
+
+}
