3c94aa039d3b0ecc5373613dea55048b5cf931a4
Majken <majros01@easv365.dk>
2025-12-01 10:56:30 +0100

    add controller

--- src/main/java/com/example/medieafspiller/myTunesController.java
+++ src/main/java/com/example/medieafspiller/myTunesController.java
@@ -1,5 +1,7 @@
 package com.example.medieafspiller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -7,8 +9,11 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+
+import javax.swing.*;
 
 public class myTunesController {
 
@@ -16,10 +21,10 @@ public class myTunesController {
     private ImageView back;
 
     @FXML
-    private TableColumn<?, ?> lenghtOfPlaylist;
+    private TableColumn<Playlist, String> lenghtOfPlaylist;
 
     @FXML
-    private TableColumn<?, ?> nameOfPlaylist;
+    private TableColumn<Playlist, String> nameOfPlaylist;
 
     @FXML
     private ImageView next;
@@ -28,40 +33,58 @@ public class myTunesController {
     private Label nowPlaying;
 
     @FXML
-    private TableColumn<?, ?> numberOfSongsOnPlaylist;
+    private TableColumn<Playlist, String> numberOfSongsOnPlaylist;
 
     @FXML
     private ImageView playPause;
 
     @FXML
-    private TableView<?> playlister;
+    private TableView<Playlist> playlister;
 
     @FXML
-    private TableColumn<?, ?> sOPArtist;
+    private TableColumn<Song, String> sOPArtist;
 
     @FXML
-    private TableColumn<?, ?> sOPLenght;
+    private TableColumn<Song, String> sOPLenght;
 
     @FXML
-    private TableColumn<?, ?> sOPName;
+    private TableColumn<Song, String> sOPName;
 
     @FXML
     private TextField searchField;
 
     @FXML
-    private TableColumn<?, ?> songArtist;
+    private TableColumn<Song, String> songArtist;
 
     @FXML
-    private TableColumn<?, ?> songLenght;
+    private TableColumn<Song, String> songLenght;
 
     @FXML
-    private TableView<?> songListe;
+    private TableView<Song> songListe = new TableView<>();
+
+    private final ObservableList<Song> songData = FXCollections.observableArrayList();
+    private final ObservableList<Playlist> playlistData = FXCollections.observableArrayList();
+    private final ObservableList<Song> sOPData = FXCollections.observableArrayList();
+
+    private void initialize() {
+        songName.setCellValueFactory(new PropertyValueFactory<Song, String>("songName"));
+        songArtist.setCellValueFactory(new PropertyValueFactory<Song, String>("songArtist"));
+        songLenght.setCellValueFactory(new PropertyValueFactory<Song, String>("songLenght"));
+
+        nameOfPlaylist.setCellValueFactory(new PropertyValueFactory<Playlist, String>("nameOfPlaylist"));
+        numberOfSongsOnPlaylist.setCellValueFactory(new PropertyValueFactory<Playlist, String>("numberOfSongsOnPlaylist"));
+        lenghtOfPlaylist.setCellValueFactory(new PropertyValueFactory<Playlist, String>("lenghtOgPlaylist"));
+
+        sOPName.setCellValueFactory(new PropertyValueFactory<Song, String>("songName"));
+        sOPArtist.setCellValueFactory(new PropertyValueFactory<Song, String>("songArtist"));
+        sOPLenght.setCellValueFactory(new PropertyValueFactory<Song, String>("songLenght"));
+    }
 
     @FXML
-    private TableColumn<?, ?> songName;
+    private TableColumn<Song, String> songName;
 
     @FXML
-    private TableView<?> songsOnPlaylist;
+    private TableView<Song> songsOnPlaylist;
 
     @FXML
     private Slider volume;
@@ -107,4 +130,5 @@ public class myTunesController {
     }
 
 }
+
 
