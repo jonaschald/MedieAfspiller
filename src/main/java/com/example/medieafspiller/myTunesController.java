82b948cc4d61208df03f4c30bf7047f65d606582
Majken <majros01@easv365.dk>
2025-12-01 11:04:48 +0100

    merged in branches

--- src/main/java/com/example/medieafspiller/myTunesController.java
+++ src/main/java/com/example/medieafspiller/myTunesController.java
@@ -1,48 +1,134 @@
 package com.example.medieafspiller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.*;
 
 public class myTunesController {
 
     @FXML
-    private TableColumn<?, ?> lenghtOfPlaylist;
+    private ImageView back;
 
     @FXML
-    private TableColumn<?, ?> nameOfPlaylist;
+    private TableColumn<Playlist, String> lenghtOfPlaylist;
 
     @FXML
-    private TableColumn<?, ?> numberOfSongsOnPlaylist;
+    private TableColumn<Playlist, String> nameOfPlaylist;
 
     @FXML
-    private TableView<?> playlister;
+    private ImageView next;
 
     @FXML
-    private TableColumn<?, ?> sOPArtist;
+    private Label nowPlaying;
 
     @FXML
-    private TableColumn<?, ?> sOPLenght;
+    private TableColumn<Playlist, String> numberOfSongsOnPlaylist;
 
     @FXML
-    private TableColumn<?, ?> sOPName;
+    private ImageView playPause;
 
     @FXML
-    private TableColumn<?, ?> songArtist;
+    private TableView<Playlist> playlister;
 
     @FXML
-    private TableColumn<?, ?> songLenght;
+    private TableColumn<Song, String> sOPArtist;
 
     @FXML
-    private TableView<?> songListe;
+    private TableColumn<Song, String> sOPLenght;
 
     @FXML
-    private TableColumn<?, ?> songName;
+    private TableColumn<Song, String> sOPName;
 
     @FXML
-    void Search(ActionEvent event) {
+    private TextField searchField;
+
+    @FXML
+    private TableColumn<Song, String> songArtist;
+
+    @FXML
+    private TableColumn<Song, String> songLenght;
+
+    @FXML
+    private TableView<Song> songListe = new TableView<>();
+
+    private final ObservableList<Song> songData = FXCollections.observableArrayList();
+    private final ObservableList<Playlist> playlistData = FXCollections.observableArrayList();
+    private final ObservableList<Song> sOPData = FXCollections.observableArrayList();
+
+    private void initialize() {
+        songName.setCellValueFactory(new PropertyValueFactory<Song, String>("songName"));
+        songArtist.setCellValueFactory(new PropertyValueFactory<Song, String>("songArtist"));
+        songLenght.setCellValueFactory(new PropertyValueFactory<Song, String>("songLenght"));
+
+        nameOfPlaylist.setCellValueFactory(new PropertyValueFactory<Playlist, String>("nameOfPlaylist"));
+        numberOfSongsOnPlaylist.setCellValueFactory(new PropertyValueFactory<Playlist, String>("numberOfSongsOnPlaylist"));
+        lenghtOfPlaylist.setCellValueFactory(new PropertyValueFactory<Playlist, String>("lenghtOgPlaylist"));
+
+        sOPName.setCellValueFactory(new PropertyValueFactory<Song, String>("songName"));
+        sOPArtist.setCellValueFactory(new PropertyValueFactory<Song, String>("songArtist"));
+        sOPLenght.setCellValueFactory(new PropertyValueFactory<Song, String>("songLenght"));
+    }
+
+    @FXML
+    private TableColumn<Song, String> songName;
+
+    @FXML
+    private TableView<Song> songsOnPlaylist;
+
+    @FXML
+    private Slider volume;
+
+    @FXML
+    void addToPlaylist(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editPlaylist(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editSong(ActionEvent event) {
+
+    }
+
+    @FXML
+    void museKlik(MouseEvent event) {
+
+    }
+
+    @FXML
+    void newPlaylist(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newSong(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search(ActionEvent event) {
 
     }
 
 }
+
+
